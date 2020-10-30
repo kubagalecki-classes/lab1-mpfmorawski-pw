196a2c64331760533a38a1f3d131f6fe2bb6df43
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-10-30 08:57:17 +0000

    Lab1 - Zadanie 1.

--- lab1.cpp
+++ lab1.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <math.h>
+
+class Wektor2D
+{
+public:
+    double x;
+    double y;
+    double norm() { return sqrt(x * x + y * y); }
+    void   print() { std::cout << norm() << '\n'; }
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Wektor2D Vector1;
+    Vector1.x = 1.12;
+    Vector1.y = 2.2;
+    Vector1.print();
 }
