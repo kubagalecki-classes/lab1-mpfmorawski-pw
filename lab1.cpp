609cfdde5f53f5aecc800c6fdd80ba6ebb18a533
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-10-31 19:59:26 +0000

    Lab1 - Zadanie 4.

--- lab1.cpp
+++ lab1.cpp
@@ -23,30 +23,47 @@ public:
         std::cout << "Wektor o wspolrzednych x = " << x << " y = " << y << " został usuniety! \n";
     }
 
-    Informer informer;
-
-    double x;
-    double y;
     double norm() { return sqrt(x * x + y * y); }
     void   print() { std::cout << norm() << "\n"; }
+
+    void setX(double vec_x) { x = vec_x; }
+    int  getX() { return x; };
+    void setY(double vec_y) { y = vec_y; }
+    int  getY() { return y; }
+
+private:
+    double x;
+    double y;
 };
 
 int main()
 {
     Wektor2D Vector1{1., 1.};
-    Wektor2D Vector2{2., 2.};
+    // std::cout << "Vector1.x = " << Vector1.x << "  Vector1.y = " << Vector1.y << " \n";
+    std::cout << "Vector1.getX() = " << Vector1.getX() << " Vector1.getY() = " << Vector1.getY()
+              << " \n";
+
+    Vector1.setX(2.);
+    std::cout << "Vector1.setX(2.); ";
+    Vector1.setY(2.);
+    std::cout << "Vector1.setY(2.); \n";
+
+    // std::cout << "Vector1.x = " << Vector1.x << "  Vector1.y = " << Vector1.y << " \n";
+    std::cout << "Vector1.getX() = " << Vector1.getX() << " Vector1.getY() = " << Vector1.getY()
+              << " \n";
+
     std::cout << "Oznaczone miejsce w kodzie \n";
 }
 
 /*
-Po wykonaniu otrzymano:
-  Konstruktor Informera
-  Wektor o wspolrzednych x = 1 y = 1 został utworzony!
-  Konstruktor Informera
-  Wektor o wspolrzednych x = 2 y = 2 został utworzony!
-  Oznaczone miejsce w kodzie
-  Wektor o wspolrzednych x = 2 y = 2 został usuniety!
-  Destruktor Informera
-  Wektor o wspolrzednych x = 1 y = 1 został usuniety!
-  Destruktor Informer
+Po wykonaniu kodu z odkomentowanymi linijkami zawierającymi Vector1.x oraz Vector1.y nie udało się
+skompilować.
+
+Po wykonaniu kodu otrzymano:
+  Wektor o wspolrzednych x = 1 y = 1 został utworzony! 
+  Vector1.getX() = 1 Vector1.getY() = 1 
+  Vector1.setX(2.); Vector1.setY(2.); 
+  Vector1.getX() = 2 Vector1.getY() = 2 
+  Oznaczone miejsce w kodzie 
+  Wektor o wspolrzednych x = 2 y = 2 został usuniety! 
 */
