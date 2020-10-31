20658fab31e2599e59b4c0c005b7259bd04a8990
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-10-31 19:31:19 +0000

    Lab1 - Zadanie 2.

--- lab1.cpp
+++ lab1.cpp
@@ -4,6 +4,18 @@
 class Wektor2D
 {
 public:
+    Wektor2D(double vec_x, double vec_y)
+    {
+        x = vec_x;
+        y = vec_y;
+        std::cout << "Wektor o wspolrzednych x = " << x << " y = " << y << " został utworzony! \n";
+    }
+
+    ~Wektor2D()
+    {
+        std::cout << "Wektor o wspolrzednych x = " << x << " y = " << y << " został usuniety! \n";
+    }
+
     double x;
     double y;
     double norm() { return sqrt(x * x + y * y); }
@@ -12,8 +24,19 @@ public:
 
 int main()
 {
-    Wektor2D Vector1;
-    Vector1.x = 1.12;
-    Vector1.y = 2.2;
-    Vector1.print();
+    Wektor2D Vector1{1., 1.};
+    Wektor2D Vector2{2., 2.};
+    Wektor2D Vector3{3., 3.};
+    std::cout << "Oznaczone miejsce w kodzie \n";
 }
+
+/*
+Po wykonaniu otrzymano:
+  Wektor o wspolrzednych x = 1 y = 1 został utworzony!
+  Wektor o wspolrzednych x = 2 y = 2 został utworzony!
+  Wektor o wspolrzednych x = 3 y = 3 został utworzony!
+  Oznaczone miejsce w kodzie
+  Wektor o wspolrzednych x = 3 y = 3 został usuniety!
+  Wektor o wspolrzednych x = 2 y = 2 został usuniety!
+  Wektor o wspolrzednych x = 1 y = 1 został usuniety!
+  */
