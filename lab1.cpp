8381a979ec6b67817181bde473c9d20495ffc1b7
Maciej Morawski <maciej.morawski2.stud@pw.edu.pl>
2020-11-01 22:47:32 +0000

    Zal

--- lab1.cpp
+++ lab1.cpp
@@ -1,43 +1,19 @@
-#include <cmath>
 #include <iostream>
-
-class Informer
-{
-public:
-    Informer() { std::cout << "Konstruktor Informera \n"; }
-    ~Informer() { std::cout << "Destruktor Informera \n"; }
-};
 
 class Wektor2D
 {
 public:
     Wektor2D()
     {
-        ++num_wek;
-        std::cout << "Wektor nr " << num_wek << " został utworzony! \n";
+        x = 0.;
+        y = 0.;
     }
 
-    ~Wektor2D()
+    Wektor2D(double vec_x, double vec_y)
     {
-        std::cout << "Wektor nr " << num_wek << " o wspolrzednych x = " << x << " y = " << y
-                  << " został usuniety!\n";
-        --num_wek;
+        x = vec_x;
+        y = vec_y;
     }
-
-    static Wektor2D kart(double kart_x, double kart_y)
-    {
-        Wektor2D Vector_kart{kart_x, kart_y};
-        return Vector_kart;
-    }
-
-    static Wektor2D bieg(double bieg_r, double bieg_fi)
-    {
-        Wektor2D Vector_bieg{bieg_r * cos(bieg_fi), bieg_r * sin(bieg_fi)};
-        return Vector_bieg;
-    }
-
-    double norm() { return sqrt(x * x + y * y); }
-    void   print() { std::cout << norm() << "\n"; }
 
     void   setX(double vec_x) { x = vec_x; }
     double getX() { return x; };
@@ -45,41 +21,34 @@ public:
     double getY() { return y; }
 
 private:
-    static int num_wek;
-
     double x;
     double y;
-
-    friend std::ostream& operator<<(std::ostream& os, const Wektor2D& Vector);
-
-    Wektor2D(double vec_x, double vec_y)
-    {
-        x = vec_x;
-        y = vec_y;
-        ++num_wek;
-        std::cout << "Wektor nr " << num_wek << " o wspolrzednych x = " << x << " y = " << y
-                  << " został utworzony! \n";
-    }
 };
 
-std::ostream& operator<<(std::ostream& os, const Wektor2D& Vector)
+Wektor2D operator+(Wektor2D Vec1, Wektor2D Vec2)
 {
-    os << "[" << Vector.x << ", " << Vector.y << "] ";
-    return os;
+    return Wektor2D{Vec1.getX() + Vec2.getX(), Vec1.getY() + Vec2.getY()};
 }
 
-int Wektor2D::num_wek = 0;
+double operator*(Wektor2D Vec1, Wektor2D Vec2)
+{
+    return ((Vec1.getX() * Vec2.getX()) + (Vec1.getY() * Vec2.getY()));
+}
 
 int main()
 {
-    Wektor2D Vector1 = Wektor2D::kart(1., 1.);
-    Wektor2D Vector2 = Wektor2D::bieg(1., 1.);
-}
+    Wektor2D v1{};         // Konstruktor domyślny, wektor o wsp. [0, 0]
+    v1.setX(1.);           // setter
+    v1.setY(1.);           // setter
+    double x1 = v1.getX(); // getter
+    std::cout << "x1 = " << x1 << std::endl;
+    double y1 = v1.getY(); // getter
+    std::cout << "y1 = " << y1 << std::endl;
+    Wektor2D v2{2., 2.}; // Konstruktor nadający współrzędne
 
-/*
-Po wykonaniu otrzymano:
-  Wektor nr 1 o wspolrzednych x = 1 y = 1 został utworzony!
-  Wektor nr 2 o wspolrzednych x = 0.540302 y = 0.841471 został utworzony!
-  Wektor nr 2 o wspolrzednych x = 0.540302 y = 0.841471 został usuniety!
-  Wektor nr 1 o wspolrzednych x = 1 y = 1 został usuniety!
-*/
+    Wektor2D sum = v1 + v2; // dodawanie wektorów
+    std::cout << "sum = [" << sum.getX() << ", " << sum.getY() << "] " << std::endl;
+
+    double prod = v1 * v2; // iloczyn skalarny
+    std::cout << "prod = " << prod << std::endl;
+}
--- zal/Wektor2D.hpp
+++ zal/Wektor2D.hpp
@@ -1,4 +1,27 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+public:
+  Wektor2D(){ x = 0.;  y = 0.; }
+
+  Wektor2D(double vec_x, double vec_y){ x = vec_x; y = vec_y; }
+
+  void   setX(double vec_x) { x = vec_x; }
+  double getX() { return x; };
+  void   setY(double vec_y) { y = vec_y; }
+  double getY() { return y; }
+
+private:
+    double x;
+    double y;
 };
+
+Wektor2D operator+(Wektor2D Vec1, Wektor2D Vec2)
+{
+    return Wektor2D{Vec1.getX() + Vec2.getX(), Vec1.getY() + Vec2.getY()};
+}
+
+double operator*(Wektor2D Vec1, Wektor2D Vec2)
+{
+    return ((Vec1.getX() * Vec2.getX()) + (Vec1.getY() * Vec2.getY()));
+}
+
